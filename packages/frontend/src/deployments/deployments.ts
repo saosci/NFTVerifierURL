cf78f8b94917b721206a6e5d4b70617e2df43683
Bug Bite <saosci@gmail.com>
2023-07-06 17:42:24 +0100

    testing deoployment

--- packages/frontend/src/deployments/deployments.ts
+++ packages/frontend/src/deployments/deployments.ts
@@ -1,26 +1,3 @@
-import { env } from '@config/environment'
-import { SubstrateDeployment } from '@scio-labs/use-inkathon'
-
 export enum ContractIds {
   Greeter = 'launchpad_psp34_nft_standard',
-}
-
-export const getDeployments = async (): Promise<SubstrateDeployment[]> => {
-  const networks = env.supportedChains
-  const contracts = Object.keys(env.nftContracts)
-  const deployments = []
-
-  for (const network of networks) {
-    for (const contract of contracts) {
-      const deployment = {
-        contractId: ContractIds.Greeter,
-        networkId: network,
-        abi: await import(`@inkathon/contracts/deployments/greeter/metadata.json`),
-        address: env.nftContracts[contract as keyof typeof env.nftContracts], // Assert `contract` as a key of `env.nftContracts`
-      }
-      deployments.push(deployment)
-    }
-  }
-
-  return deployments
 }
