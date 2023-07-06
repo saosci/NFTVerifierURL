6791cdc53f0aee4f593d56c8c2a6397fbce0b500
Bug Bite <saosci@gmail.com>
2023-07-06 08:55:14 +0100

    testing deoployment address

--- packages/frontend/src/deployments/deployments.ts
+++ packages/frontend/src/deployments/deployments.ts
@@ -5,15 +5,21 @@ export enum ContractIds {
   Greeter = 'launchpad_psp34_nft_standard',
 }
 
-export const getDeployments = async (): Promise<SubstrateDeployment[]> => {
+export const getDeployments = async (guildId: string): Promise<SubstrateDeployment[]> => {
   const networks = env.supportedChains
+
+  // Fetch the contract address for the given guildId from your backend
+  const response = await fetch(`https://api.op2.app/get-latest-message/${guildId}`)
+  const data = await response.json()
+  const contractAddress = data.ContractIds
+
   const deployments = networks
     .map(async (network) => [
       {
         contractId: ContractIds.Greeter,
         networkId: network,
         abi: await import(`@inkathon/contracts/deployments/greeter/metadata.json`),
-        address: (await import(`@inkathon/contracts/deployments/greeter/${network}.ts`)).address,
+        address: contractAddress, // Use the fetched contract address
       },
     ])
     .reduce(async (acc, curr) => [...(await acc), ...(await curr)], [] as any)
