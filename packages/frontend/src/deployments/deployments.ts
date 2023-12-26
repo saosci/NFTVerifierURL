85fcf740a67e23c1f11f3a482457e7129d1a0c2c
Bug Bite <saosci@gmail.com>
2023-12-26 14:57:01 +0000

    updated deployment

--- packages/frontend/src/deployments/deployments.ts
+++ packages/frontend/src/deployments/deployments.ts
@@ -1,21 +1,25 @@
-import { env } from '@config/environment'
-import { SubstrateDeployment } from '@scio-labs/use-inkathon'
+import { env } from '@config/environment';
+import { SubstrateDeployment } from '@scio-labs/use-inkathon';
 
 export enum ContractIds {
-  Greeter = 'launchpad_psp34_nft_standard',
+  Greeter = 'launchpad_psp34_nft_standard', // Your contract ID
 }
 
 export const getDeployments = async (contractAddress: string): Promise<SubstrateDeployment[]> => {
-  const networks = env.supportedChains
-  const deployments = networks
-    .map(async (network) => [
-      {
-        contractId: ContractIds.Greeter,
-        networkId: network,
-        abi: await import(`@inkathon/contracts/deployments/greeter/metadata.json`),
-        address: contractAddress,
-      },
-    ])
-    .reduce(async (acc, curr) => [...(await acc), ...(await curr)], [] as any)
-  return deployments
-}
+  const networks = env.supportedChains;
+  const deployments: SubstrateDeployment[] = [];
+
+  for (const networkId of networks) {
+    // Assuming you want to use the same contract address for each network
+    const abi = await import(`@inkathon/contracts/deployments/${ContractIds.Greeter}/metadata.json`);
+
+    deployments.push({ 
+      contractId: ContractIds.Greeter, 
+      networkId, 
+      abi, 
+      address: contractAddress // Using the passed contractAddress
+    });
+  }
+
+  return deployments;
+};
