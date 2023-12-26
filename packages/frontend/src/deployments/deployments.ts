e0d2dc48efb82a24b3f8898d70e2fa938e73233e
Bug Bite <saosci@gmail.com>
2023-12-26 14:24:01 +0000

    testing new method

--- packages/frontend/src/deployments/deployments.ts
+++ packages/frontend/src/deployments/deployments.ts
@@ -6,16 +6,42 @@ export enum ContractIds {
 }
 
 export const getDeployments = async (contractAddress: string): Promise<SubstrateDeployment[]> => {
-  const networks = env.supportedChains
-  const deployments = networks
-    .map(async (network) => [
-      {
-        contractId: ContractIds.Greeter,
-        networkId: network,
-        abi: await import(`@inkathon/contracts/deployments/greeter/metadata.json`),
-        address: contractAddress,
-      },
-    ])
-    .reduce(async (acc, curr) => [...(await acc), ...(await curr)], [] as any)
-  return deployments
+  console.log('getDeployments called with contractAddress:', contractAddress);
+
+  // Fetch the supported networks from the environment configuration
+  const networks = env.supportedChains;
+  console.log('Supported networks:', networks);
+
+  // Map each network to a deployment configuration
+  const deploymentsPromises = networks.map(async (network) => {
+    console.log('Processing network:', network);
+
+    // Dynamically import the ABI for the contract
+    const abi = await import(`@inkathon/contracts/deployments/greeter/metadata.json`);
+    console.log('ABI loaded for network:', network, abi);
+
+    return [{
+      contractId: ContractIds.Greeter,
+      networkId: network,
+      abi: abi,
+      address: contractAddress,
+    }];
+  });
+
+  console.log('Deployment promises created:', deploymentsPromises);
+
+  // Resolve all promises and flatten the array of deployments
+  const deployments = await deploymentsPromises.reduce(async (acc, curr) => {
+    const accumulated = await acc;
+    console.log('Accumulated deployments so far:', accumulated);
+
+    const current = await curr;
+    console.log('Current deployment:', current);
+
+    return [...accumulated, ...current];
+  }, Promise.resolve([] as SubstrateDeployment[]));
+
+  console.log('Final deployments:', deployments);
+
+  return deployments;
 }
