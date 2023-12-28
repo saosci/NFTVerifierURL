eff078a4d7287c0b2483678fe495aeec09b6cff7
Bug Bite <saosci@gmail.com>
2023-12-28 10:54:03 +0000

    padding

--- packages/frontend/src/pages/index.tsx
+++ packages/frontend/src/pages/index.tsx
@@ -20,7 +20,7 @@ const HomePage: NextPage = () => {
 
   return (
     <>
-    <HomeTopBar style={{ paddingBottom: '2.5rem' }}/>
+    <HomeTopBar tw="mb-10 px-5"/>
     <CenterBody tw="sm:mt-5 md:mt-10 lg:mt-20 mb-10 px-5">
         {/* Title */}
         <LandingPage />
